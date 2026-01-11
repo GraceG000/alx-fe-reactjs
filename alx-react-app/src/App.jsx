578f35dc74b31a80ca4cc60ffa4dbfc5252d19a5
Grace Ghansah <graceghansah2000@gmail.com>
2026-01-11 18:08:11 +0000

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage.jsx';
+import UserProfile from './components/UserProfile.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import MainContent from './components/MainContent.jsx';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -30,6 +34,15 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile
+      name="Alice"
+      age="25"
+      bio="Loves hiking and photography"
+      />
+      <Footer />
+      
     </>
   )
 }
