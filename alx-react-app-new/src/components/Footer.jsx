472f1068c3c0bb830146b52c870fa38daa547909
Grace Ghansah <graceghansah2000@gmail.com>
2026-01-17 23:24:10 +0000

    added alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <>
+        <footer style= {{ textAlign: 'center', padding: '10px', color: 'white', backgroundColor: 'navy' }}>
+            <p>&copy; 2023 City Lovers</p>
+        </footer>
+        </>
+    )
+}
+export default Footer;
