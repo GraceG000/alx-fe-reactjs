18d991f11cfe02014ac9138cf413f8d5828ae432
Grace Ghansah <graceghansah2000@gmail.com>
2026-01-18 00:24:13 +0000

    update

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navStyle = {
+    display: "flex",
+    gap: "15px",
+    backgroundColor: "#0d47a1",
+    padding: "15px"
+  };
+
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold"
+  };
+
+  return (
+    <nav style={navStyle}>
+      <Link style={linkStyle} to="/">Home</Link>
+      <Link style={linkStyle} to="/about">About</Link>
+      <Link style={linkStyle} to="/services">Services</Link>
+      <Link style={linkStyle} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
