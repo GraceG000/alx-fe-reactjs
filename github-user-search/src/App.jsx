e605c39a76da29d749d3b2b3aa1c472d81f8dbf4
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-01 00:35:05 +0000

    initial commit for the github user search project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return(
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>GitHub User Search</h1>
+      <p>Search for GitHub users and view their profiles.</p>
+    </div>
+  )
+}
+export default App;
