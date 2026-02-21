d91d439531df9316ff8df7a850208da37181ed95
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-21 21:34:00 +0000

    update

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function TodoList() {
+const TodoList = () => {
 
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", completed: false },
     { id: 2, text: "Write Tests", completed: false }
   ]);
 
+  const [input, setInput] = useState("");
 
-  const addTodo = (text) => {
+
+  const addTodo = () => {
+
+    if (!input) return;
 
     const newTodo = {
       id: Date.now(),
-      text: text,
+      text: input,
       completed: false
     };
 
     setTodos([...todos, newTodo]);
+    setInput("");
   };
 
 
@@ -48,27 +53,17 @@ function TodoList() {
 
       <h1>Todo List</h1>
 
-
       <input
-        type="text"
         placeholder="Add Todo"
-        id="todoInput"
+        value={input}
+        onChange={(e) =>
+          setInput(e.target.value)
+        }
       />
 
-
-      <button
-        onClick={() => {
-          const input =
-            document.getElementById("todoInput");
-
-          addTodo(input.value);
-
-          input.value = "";
-        }}
-      >
+      <button onClick={addTodo}>
         Add
       </button>
-
 
 
       <ul>
@@ -78,7 +73,9 @@ function TodoList() {
           <li key={todo.id}>
 
             <span
-              onClick={() => toggleTodo(todo.id)}
+              onClick={() =>
+                toggleTodo(todo.id)
+              }
               style={{
                 textDecoration:
                   todo.completed
@@ -98,7 +95,6 @@ function TodoList() {
               Delete
             </button>
 
-
           </li>
 
         ))}
@@ -110,6 +106,6 @@ function TodoList() {
 
   );
 
-}
+};
 
 export default TodoList;
